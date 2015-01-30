c2ed09d8d6f3afcefae65edde03b36b3e55b63d7
= <=>
2015-01-30 12:22:32 -0500

    init

---
+++ public/javascripts/AngularApp.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var app = angular.module('app', ['ngRoute', 'appControllers', 'appServices', 'appDirectives']);
+
+var appServices = angular.module('appServices', []);
+var appControllers = angular.module('appControllers', []);
+var appDirectives = angular.module('appDirectives', []);
